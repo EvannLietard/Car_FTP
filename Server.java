b55f83d4a3ca40d5b32ba6cfac8272f12e6649b1
EvannLietard <evann59167@gmail.com>
2024-12-11 17:12:33 +0100

    Need to a real password and login

--- Server.java
+++ Server.java
@@ -6,21 +6,42 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
-    public static void main(String[] args) throws IOException{
-        ServerSocket serv=new ServerSocket(2024);
-        Socket s2=serv.accept();
+    public static void main(String[] args){
+        try {
+            ServerSocket serv=new ServerSocket(2024);
 
-        OutputStream out= s2.getOutputStream();
-        String str= "220 Service ready";
-        out.write(str.getBytes());
+                Socket s2=serv.accept();
+                OutputStream out= s2.getOutputStream();
+                String str= "220 Service ready \r\n";
+                out.write(str.getBytes());
+                InputStream in= s2.getInputStream();
+                Scanner scan = new Scanner(in);
+            boolean isAuthenticated = false;
+            while (true) {
+                String command = scan.nextLine();
+                System.out.println("Reçu: " + command);
+                if (!isAuthenticated) {
+                    if (command.startsWith("USER ")) {
+                        out.write("331 Password required\r\n".getBytes());
+                    } else if (command.startsWith("PASS ")) {
+                        isAuthenticated = true;
+                        out.write("230 User logged in, proceed\r\n".getBytes());
+                    } else {
+                        out.write("530 Please login with USER and PASS\r\n".getBytes());
+                    }
+                } else {
+                    if (command.equalsIgnoreCase("QUIT")) {
+                        break;
+                    } else {
+                        out.write("502 Command not implemented\r\n".getBytes());
+                    }
+                }
+            }
 
-        InputStream in= s2.getInputStream();
-        Scanner scan= new Scanner(in);
-        String strs = scan.nextLine();
+        }
+        catch (IOException e){
+            System.out.println("Erreur");
+        }
 
-        System.out.println("in" + str);
-
-        s2.close();
-        serv.close();
     }
 }
