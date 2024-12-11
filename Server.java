d4a37c3e35a0a78c50f243154345fd8e97c97cb3
EvannLietard <evann59167@gmail.com>
2024-12-11 17:19:33 +0100

    Good with a hard password and login

--- Server.java
+++ Server.java
@@ -6,6 +6,8 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
+    private static final String LOGIN = "Evann";
+    private static final String PASSWORD = "LIETARD";
     public static void main(String[] args){
         try {
             ServerSocket serv=new ServerSocket(2024);
@@ -17,15 +19,22 @@ public class Server {
                 InputStream in= s2.getInputStream();
                 Scanner scan = new Scanner(in);
             boolean isAuthenticated = false;
+            String receivedLogin=null;
             while (true) {
                 String command = scan.nextLine();
                 System.out.println("Reçu: " + command);
                 if (!isAuthenticated) {
                     if (command.startsWith("USER ")) {
+                        receivedLogin = command.substring(5).trim();
                         out.write("331 Password required\r\n".getBytes());
                     } else if (command.startsWith("PASS ")) {
-                        isAuthenticated = true;
-                        out.write("230 User logged in, proceed\r\n".getBytes());
+                        String receivedPassword = command.substring(5).trim();
+                        if (LOGIN.equals(receivedLogin) && PASSWORD.equals(receivedPassword)) {
+                            isAuthenticated = true;
+                            out.write("230 User logged in, proceed\r\n".getBytes());
+                        } else {
+                            out.write("530 Login incorrect\r\n".getBytes());
+                        }
                     } else {
                         out.write("530 Please login with USER and PASS\r\n".getBytes());
                     }
